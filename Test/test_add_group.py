12bb5490a267cf5318f30dbb6adc7a9198a46db7
Serj2004 <korolevsn@it2g.ru>
2021-06-22 19:50:15 +0700

    conftest file and delete group test added

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -1,13 +1,4 @@
-from Fixture.application import Application
-import pytest
 from Model.group import Group
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_group(app):
