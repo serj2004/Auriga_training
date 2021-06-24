bef720664829e9ac57c3377f88e6cfce3157fd40
Serj2004 <korolevsn@it2g.ru>
2021-06-24 17:03:27 +0700

    create and edit contact methods refactored

--- Model/contact.py
+++ Model/contact.py
@@ -2,8 +2,10 @@
 
 class Contact:
 
-    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, home, mobile, work,
-                 fax, email, email2, email3, homepage, bday, bmonth, byear, address2, phone2, notes):
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, photo=None, title=None,
+                 company=None, address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None,
+                 email3=None, homepage=None, bday=None, bmonth=None, byear=None, address2=None, phone2=None,
+                 notes=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
--- Test/test_edit_contact.py
+++ Test/test_edit_contact.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
 from Model.contact import Contact
 
 
-def test_add_contact(app):
+def test_edit_first_contact(app):
     app.contact.edit_first_contact(Contact(firstname='1', middlename='1', lastname='1',
-                                           photo=os.path.join(os.path.join(os.getcwd(), 'photo'), 'eo4qjv87_thumb.jpg'),
                                            nickname='1', title='1', company='1', address='1',
                                            home='1', mobile='1', work='1', fax='1',
                                            email='1', email2='1', email3='1',
-                                           homepage='1', bday='2', bmonth='February', byear='1',
+                                           homepage='1', byear='1',
                                            address2='1', phone2='1', notes='1'))
 
