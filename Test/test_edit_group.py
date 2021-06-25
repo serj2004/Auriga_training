835b94ae99d33478f415268acda7f3f28fdf36fc
Serj2004 <korolevsn@it2g.ru>
2021-06-25 17:38:50 +0700

    comparison of old and new groups added

--- Test/test_edit_group.py
+++ Test/test_edit_group.py
@@ -5,10 +5,16 @@ from Model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="group2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="group2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
