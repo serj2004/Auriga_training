997aacd7254eef86ff19101faadfc6d13d463da4
Serj2004 <korolevsn@it2g.ru>
2021-06-30 00:19:50 +0700

    fixture options added

--- Fixture/application.py
+++ Fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from Fixture.contact import ContactHelper
 from Fixture.group import GroupHelper
 from Fixture.session import SessionHelper
@@ -6,13 +6,21 @@ from Fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.open_home_page()
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,9 +31,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        url = "https://localhost/addressbook/"
-        if not (wd.current_url == url):
-            wd.get(url)
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- Test/test_add_contact.py
+++ Test/test_add_contact.py
@@ -18,8 +18,8 @@ test_data = [Contact(firstname="", lastname="")] + [Contact(firstname=random_str
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
-    new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
