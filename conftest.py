f86b9e79345a586899d0a7a810e55cceb3614746
Serj2004 <korolevsn@it2g.ru>
2021-06-30 15:46:58 +0700

    user/pass option added

--- conftest.py
+++ conftest.py
@@ -10,8 +10,10 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--user")
+    password = request.config.getoption("--pass")
     if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
+        fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
     else:
         if not fixture.is_valid():
             fixture = Application(browser=browser, base_url=base_url)
@@ -31,3 +33,5 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--user", action="store", default=None)
+    parser.addoption("--pass", action="store", default=None)
