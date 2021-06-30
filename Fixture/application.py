d9983cea386fad9f582f9338a87203591a6e7457
Serj2004 <korolevsn@it2g.ru>
2021-06-30 17:12:27 +0700

    config file added

--- Fixture/application.py
+++ Fixture/application.py
@@ -6,7 +6,7 @@ from Fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self, browser, base_url, username, password):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -15,16 +15,12 @@ class Application:
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
-        if username != "admin":
-            raise ValueError("Username is not valid!")
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
         self.open_home_page()
-        self.username = username
-        self.password = password
 
     def is_valid(self):
         try:
--- conftest.py
+++ conftest.py
@@ -1,23 +1,23 @@
 from Fixture.application import Application
 import pytest
+import json
 
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    username = request.config.getoption("--user")
-    password = request.config.getoption("--pass")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
-    fixture.session.ensure_login(username='admin', password='secret')
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -32,6 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--user", action="store", default=None)
-    parser.addoption("--pass", action="store", default=None)
+    parser.addoption("--target", action="store", default="target.json")
