ea091e1996415536f04b7071f0b2c9869683e3c1
Sergey <serg_2004@inbox.ru>
2021-07-01 22:51:57 +0700

    full name check was divided

--- Test/test_contact_on_home_and_edit_page.py
+++ Test/test_contact_on_home_and_edit_page.py
@@ -4,8 +4,8 @@ import re
 def test_info_on_home_and_edit_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_from_edit_page(0)
-    assert contact_from_home_page.firstname, contact_from_home_page.lastname == \
-           merge_names_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
     assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
@@ -29,11 +29,6 @@ def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None,
                                                                               [contact.homephone, contact.mobilephone,
                                                                                contact.workphone, contact.phone2]))))
-
-
-def merge_names_like_on_home_page(contact):
-    return "".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None,
-                                                                            [contact.firstname, contact.lastname]))))
 
 
 def clear(s):
