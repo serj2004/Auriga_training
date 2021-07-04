c3c5422debd2d35cab8f1ae96a404f64dfbcff8d
Sergey <serg_2004@inbox.ru>
2021-07-04 15:29:01 +0700

    getting contact list method added

---
+++ check_db_connection.py
@@ -0,0 +1,10 @@
+from Fixture.db import DbFixture
+
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+try:
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
+finally:
+    db.destroy()
