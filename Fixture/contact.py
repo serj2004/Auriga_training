98b00d9dca3da7f6c41e494bda3128c97868b772
Sergey <serg_2004@inbox.ru>
2021-08-03 22:50:18 +0700

    test_info_on_homepage_and_db fixed

--- Fixture/contact.py
+++ Fixture/contact.py
@@ -1,6 +1,4 @@
 import re
-
-from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
