24061f17e823a19cb89762c53dd11bb5aebd68b4
Sergey <serg_2004@inbox.ru>
2021-08-07 13:08:52 +0700

    test_info_on_homepage_and_db fixed

--- Test/test_contact_on_home_and_edit_page.py
+++ Test/test_contact_on_home_and_edit_page.py
@@ -1,6 +1,4 @@
 import re
-
-from Fixture import contact
 from Model.contact import Contact
 
 
@@ -18,8 +16,8 @@ def test_info_on_home_and_edit_page(app):
     contact_from_edit_page = app.contact.get_contact_from_edit_page_by_index(0)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.all_phones_from_home_page == merge_data_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_email_from_home_page == merge_data_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
     assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
 
 
@@ -38,7 +36,7 @@ def merge_email_like_on_home_page(contact):
 
 
 def merge_data_like_on_home_page(list):
-    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, list)))
+    return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, list))))
 
 
 def merge_phones_like_on_home_page(contact):
@@ -48,4 +46,4 @@ def merge_phones_like_on_home_page(contact):
 
 
 def clear(s):
-    return re.sub("[() +-]|\n", "", s)
+    return re.sub("[() +-]", "", s)
