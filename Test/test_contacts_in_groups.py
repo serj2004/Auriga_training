5b8def01e0cc9f57d62160e4fd4ee56e25acc87e
Sergey <serg_2004@inbox.ru>
2021-08-08 12:03:24 +0700

    test add/delete contact to/from group fixed

--- Test/test_contacts_in_groups.py
+++ Test/test_contacts_in_groups.py
@@ -9,8 +9,9 @@ def test_add_contact_into_group(app, db):
     group_list = db.get_group_list()
     if len(group_list) == 0:
         app.group.create(Group(name='test'))
-    contact_add_to_group = old_contacts_not_in_groups[0]
-    app.contact.add_contact_to_group(contact_add_to_group, group_list[0].id)
+    contact_add_to_group = db.get_contacts_not_in_groups()[0]
+    group = db.get_group_list()[0].id
+    app.contact.add_contact_to_group(contact_add_to_group, group)
     new_contacts_not_in_groups = db.get_contacts_not_in_groups()
     assert contact_add_to_group not in new_contacts_not_in_groups
 
@@ -20,12 +21,13 @@ def test_delete_contact_from_group(app, db):
     if len(contact_list) == 0:
         app.contact.create(Contact(lastname='1', firstname='2', bday='1', bmonth='January'))
     group_list = app.group.get_group_list()
-    group_name = group_list[0].name
     if len(group_list) == 0:
         app.group.create(Group(name='test'))
+    group_name = app.group.get_group_list()[0].name
     old_contacts_not_in_groups = db.get_contacts_not_in_groups()
     contact_delete_from_group = old_contacts_not_in_groups[0]
-    app.contact.add_contact_to_group(contact_delete_from_group, group_list[0].id)
+    group_id = app.group.get_group_list()[0].id
+    app.contact.add_contact_to_group(contact_delete_from_group, group_id)
     id = app.contact.delete_contact_from_group(contact_delete_from_group, group_name)
     new_contacts_not_in_groups = db.get_contacts_not_in_groups()
     assert id in new_contacts_not_in_groups
